2fa722a7cdf77358a5a796e83e786ed83d6ecb92
Alexander Karankevich <givmemp3@gmail.com>
2021-11-29 00:22:29 +0300

    refactor and fix main view add login page add notification in chat window when user joined add user name to message content

--- src/main/java/by/ak/chat/MainView.java
+++ src/main/java/by/ak/chat/MainView.java
@@ -1,41 +1,125 @@
 package by.ak.chat;
 
+import com.github.rjeschke.txtmark.Processor;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.Registration;
+import lombok.Getter;
+import lombok.Setter;
 
-@Route("")
+import java.util.Objects;
+
+@Route("/")
+@Push
 public class MainView extends VerticalLayout {
+  public static final String CHAT_MESSAGE_TEMPLATE = "**%s**: %s";
+  private final Grid<ChatMessage> grid;
   private final Storage storage;
+  private Registration registration;
+  private VerticalLayout chat;
+  private VerticalLayout login;
+  @Getter
+  @Setter
+  private String user;
 
   public MainView(Storage storage) {
     this.storage = storage;
 
-    var grid = new Grid<Storage.ChatMessage>();
+    buildLogin();
+    grid = buildChatGrid(storage);
+  }
+
+  private void buildLogin() {
+    login = new VerticalLayout();
+    login.setVisible(true);
+    var username = new TextField("Username");
+    username.setAutofocus(true);
+    login.add(username, new Button("Login") {
+      {
+        addClickListener(click -> {
+          var user = username.getValue();
+          setUser(user);
+          login.setVisible(false);
+          chat.setVisible(true);
+          storage.addMessageUserJoined(String.format("**%s** joined", user));
+        });
+        addClickShortcut(Key.ENTER);
+      }
+    });
+
+    add(login);
+  }
+
+  private Grid<ChatMessage> buildChatGrid(Storage storage) {
+    chat = new VerticalLayout();
+    chat.setVisible(false);
+    add(chat);
+    final Grid<ChatMessage> grid;
+    grid = new Grid<>();
     grid.setItems(storage.getMessages());
-    grid.addColumn(new ComponentRenderer<>(message -> new Html(message.getMessage())))
-        .setAutoWidth(true);
+    grid.addColumn(new ComponentRenderer<>(message -> new Html(renderRow(message))))
+      .setAutoWidth(true);
     var textField = new TextField();
     textField.setAutofocus(true);
-    add(
-        new H3("Chat"),
-        grid,
-        textField,
-        new Button("Send message") {
-          {
-            addClickListener(
-                click -> {
-                  storage.addMessage("", textField.getValue());
-                  textField.clear();
-                });
-            addClickShortcut(Key.ENTER);
-          }
-        });
+    chat.add(
+      new H3("Chat"),
+      grid,
+      new HorizontalLayout() {
+        {
+          add(
+            textField,
+            new Button("➤") {
+              {
+                addClickListener(
+                  click -> {
+                    storage.addMessage(user, textField.getValue());
+                    textField.clear();
+                  });
+                addClickShortcut(Key.ENTER);
+              }
+            });
+        }
+      });
+    return grid;
+  }
+
+  public void onMessage(Storage.ChatEvent event) {
+    getUI()
+      .ifPresent(
+        ui ->
+          ui.access(
+            () -> {
+              grid.getDataProvider().refreshAll();
+              getUI().ifPresent(UI::push);
+              ui.beforeClientResponse(grid, ctx -> grid.scrollToEnd());
+            }));
+  }
+
+  @Override
+  protected void onAttach(AttachEvent attachEvent) {
+    registration = storage.attachListener(this::onMessage);
+  }
+
+  @Override
+  protected void onDetach(DetachEvent detachEvent) {
+    registration.remove();
+  }
+
+  private String renderRow(ChatMessage message) {
+    if (Objects.isNull(message.getUser())) {
+      return Processor.process(message.getText());
+    } else return Processor.process(String.format(CHAT_MESSAGE_TEMPLATE, message.getUser(), message.getText()));
   }
 }
--- src/main/java/by/ak/chat/Storage.java
+++ src/main/java/by/ak/chat/Storage.java
@@ -1,6 +1,10 @@
 package by.ak.chat;
 
-import lombok.AllArgsConstructor;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventBus;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.shared.Registration;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
@@ -10,15 +14,28 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @Component
 public class Storage {
   @Getter
-  private Queue<ChatMessage> messages = new ConcurrentLinkedQueue<>();
+  private final Queue<ChatMessage> messages = new ConcurrentLinkedQueue<>();
+  private final ComponentEventBus eventBus = new ComponentEventBus(new Div());
 
   public void addMessage(String user, String message) {
-    messages.add(new ChatMessage(user, message));}
+    messages.add(new ChatMessage(user, message));
+    eventBus.fireEvent(new ChatEvent());
+  }
 
-  @Getter
-  @AllArgsConstructor
-  public static class ChatMessage {
-    private String user;
-    private String message;
+  public void addMessageUserJoined(String message) {
+    var userJoined = new ChatMessage();
+    userJoined.setText(message);
+    messages.add(userJoined);
+    eventBus.fireEvent(new ChatEvent());
+  }
+
+  public static class ChatEvent extends ComponentEvent<Div> {
+    public ChatEvent() {
+      super(new Div(), false);
+    }
+  }
+
+  public Registration attachListener(ComponentEventListener<ChatEvent> listener) {
+    return eventBus.addListener(ChatEvent.class, listener);
   }
 }
