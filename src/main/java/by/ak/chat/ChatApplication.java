50139bbea01ca9178398b1cd97417d7ca540c329
Alexander Karankevich <givmemp3@gmail.com>
2021-11-30 03:19:33 +0300

    add login view add spring security

--- src/main/java/by/ak/chat/ChatApplication.java
+++ src/main/java/by/ak/chat/ChatApplication.java
@@ -1,9 +1,12 @@
 package by.ak.chat;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
 public class ChatApplication {
 
   public static void main(String[] args) {
--- src/main/java/by/ak/chat/LoginView.java
+++ src/main/java/by/ak/chat/LoginView.java
@@ -9,7 +9,7 @@ import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route("login")
+@Route("/")
 @PageTitle("Login")
 public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
