48b6d0ae5bf2a80d0cc92800bcef56165733b328
Alexander Karankevich <givmemp3@gmail.com>
2021-12-08 00:51:12 +0300

    update ChatViewV2

--- src/main/java/by/ak/chat/view/ChatViewV2.java
+++ src/main/java/by/ak/chat/view/ChatViewV2.java
@@ -1,50 +1,89 @@
 package by.ak.chat.view;
 
+import by.ak.chat.model.Storage;
 import by.ak.chat.security.SecurityService;
-//import com.vaadin.collaborationengine.CollaborationAvatarGroup;
-//import com.vaadin.collaborationengine.UserInfo;
+import by.ak.chat.util.DateTimeProvider;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.messages.MessageInput;
 import com.vaadin.flow.component.messages.MessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route("chatV2")
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Route(ChatViewV2.PATH)
 @PageTitle("Chat V2")
-@Push
 public class ChatViewV2 extends VerticalLayout {
 
-//  private final UserInfo userInfo;
+  public static final String PATH = "/chatV2";
+  public static final String TITLE = "Chat";
   private final SecurityService securityService;
+  private final Storage storage;
+  private final DateTimeProvider dateTimeProvider;
 
-  public ChatViewV2(SecurityService securityService) {
+  public ChatViewV2(SecurityService securityService, Storage storage, DateTimeProvider dateTimeProvider) {
     this.securityService = securityService;
-//    this.userInfo = new UserInfo(securityService.getLoggedInUserName());
+    this.storage = storage;
+    this.dateTimeProvider = dateTimeProvider;
 
     addAndExpand(getHeader());
-    addAndExpand(getMessageList());
+
+    MessageList list = new MessageList();
+    initMessageList(list);
+    MessageInput input = getMessageInput(list);
+    VerticalLayout chatLayout = getChatLayout(list, input);
+
+    add(chatLayout);
   }
 
   private Component getHeader() {
     HorizontalLayout header = new HorizontalLayout();
     header.setWidthFull();
     header.setAlignItems(HorizontalLayout.Alignment.BASELINE);
-
-//    CollaborationAvatarGroup avatars = new CollaborationAvatarGroup(userInfo, "avatars");
-
-    H1 title = new H1("Chat");
-
-//    header.add(title, avatars);
+    H1 title = new H1(TITLE);
     header.addAndExpand(title);
     return header;
   }
 
-  private Component getMessageList() {
-    MessageList messageList = new MessageList();
-    messageList.setWidthFull();
-    return messageList;
+  private void initMessageList(MessageList messageList) {
+    List<MessageListItem> messageListItems = storage.getMessages().stream().map(message ->
+        new MessageListItem(
+          message.getText(),
+          dateTimeProvider.format(message.getCreated()),
+          message.getUser()))
+      .collect(Collectors.toList());
+    messageList.setItems(messageListItems);
+  }
+
+  private MessageInput getMessageInput(MessageList list) {
+    MessageInput input = new MessageInput();
+    input.addSubmitListener(submitEvent -> {
+      MessageListItem newMessage = new MessageListItem(
+        submitEvent.getValue(), Instant.now(), securityService.getLoggedInUserName());
+      newMessage.setUserColorIndex(new Random().nextInt(3));
+      List<MessageListItem> items = new ArrayList<>(list.getItems());
+      items.add(newMessage);
+      list.setItems(items);
+      storage.addMessage(newMessage);
+    });
+    return input;
+  }
+
+  private VerticalLayout getChatLayout(MessageList list, MessageInput input) {
+    VerticalLayout chatLayout = new VerticalLayout(list, input);
+    chatLayout.setHeight(60, Unit.PERCENTAGE);
+    chatLayout.setWidth(70, Unit.PERCENTAGE);
+    chatLayout.expand(list);
+    return chatLayout;
   }
 }
