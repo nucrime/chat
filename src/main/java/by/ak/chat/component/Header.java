af977df7d10743a33f9179cf3585ed03d8d039b8
Alexander Karankevich <givmemp3@gmail.com>
2021-12-12 04:46:35 +0300

    update header section

--- src/main/java/by/ak/chat/component/Header.java
+++ src/main/java/by/ak/chat/component/Header.java
@@ -1,11 +1,13 @@
 package by.ak.chat.component;
 
 import by.ak.chat.security.SecurityService;
+import by.ak.chat.view.ChatView;
 import by.ak.chat.view.UserView;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,12 @@ import static by.ak.chat.view.ChatView.LOG_OUT;
 @SessionScope
 @RequiredArgsConstructor
 public class Header {
+  public static final String CHAT = "Chat";
   private final SecurityService securityService;
 
   public VerticalLayout init() {
-    VerticalLayout header = new VerticalLayout();
+    VerticalLayout container = new VerticalLayout();
+    HorizontalLayout header = new HorizontalLayout();
     UI current = UI.getCurrent();
 
     if (securityService.isAdmin()) {
@@ -28,11 +32,16 @@ public class Header {
       header.add(management);
     }
 
+    Button chatButton = new Button(CHAT, e -> current.navigate(ChatView.PATH));
+    header.add(chatButton);
+
     Button logoutButton = new Button(LOG_OUT, e -> securityService.logout());
     header.add(logoutButton);
-    header.setHeight(10, Unit.PERCENTAGE);
-    header.setAlignItems(FlexComponent.Alignment.END);
 
-    return header;
+    container.setHeight(10, Unit.PERCENTAGE);
+    container.setAlignItems(FlexComponent.Alignment.END);
+    container.add(header);
+
+    return container;
   }
 }
