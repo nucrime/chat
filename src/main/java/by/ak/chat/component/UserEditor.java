c1c5b2a55db127571d561fe07d4df6ffaa643c75
Alexander Karankevich <givmemp3@gmail.com>
2021-12-12 04:07:07 +0300

    add user editor

--- src/main/java/by/ak/chat/component/UserEditor.java
+++ src/main/java/by/ak/chat/component/UserEditor.java
@@ -1,29 +1,114 @@
 package by.ak.chat.component;
 
+import by.ak.chat.model.User;
 import by.ak.chat.service.UserService;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.UIScope;
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 
-@SpringComponent
+@Component
 @UIScope
-@RequiredArgsConstructor
 public class UserEditor extends VerticalLayout implements KeyNotifier {
   private final UserService userService;
+  private User user;
 
   TextField username = new TextField("Username");
-  TextField password = new TextField("Email");
+  TextField email = new TextField("Email");
   TextField firstName = new TextField("First name");
   TextField lastName = new TextField("Last name");
   TextField role = new TextField("Role");
+  TextField password = new TextField("Password");
 
-  private Button save = new Button("Save");
-  private Button update = new Button("Update");
-  private Button delete = new Button("Delete");
+  private Button save = new Button("Save", VaadinIcon.CHECK.create());
+  private Button cancel = new Button("Cancel", VaadinIcon.CLOSE_SMALL.create());
+  private Button delete = new Button("Delete", VaadinIcon.TRASH.create());
+  private HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
+  private Binder<User> binder = new Binder<>(User.class);
+  private ChangeHandler changeHandler;
+
+  public UserEditor(UserService userService) {
+    this.userService = userService;
+    HorizontalLayout firstRow = new HorizontalLayout();
+    firstRow.add(firstName, lastName, username);
+    HorizontalLayout secondRow = new HorizontalLayout();
+    secondRow.add(email, role, password);
+    HorizontalLayout thirdRow = new HorizontalLayout();
+    thirdRow.add(actions);
+    add(firstRow, secondRow, thirdRow);
+
+    // bind using naming convention
+    binder.bindInstanceFields(this);
+
+    // Configure and style components
+    setSpacing(true);
+
+    save.getElement().getThemeList().add("primary");
+    delete.getElement().getThemeList().add("error");
+
+    addKeyPressListener(Key.ENTER, e -> save());
+
+    // wire action buttons to save, delete and reset
+    save.addClickListener(e -> save());
+    delete.addClickListener(e -> delete());
+    cancel.addClickListener(e -> editCustomer(user));
+    setVisible(false);
+  }
+
+  void delete() {
+    userService.delete(user);
+    changeHandler.onChange();
+  }
+
+  void save() {
+    userService.save(user);
+    changeHandler.onChange();
+  }
+
+  public interface ChangeHandler {
+    void onChange();
+  }
+
+  public final void editCustomer(User u) {
+    // if user is null, don't show form and actions
+    if (u == null) {
+      setVisible(false);
+      return;
+    }
+    final boolean persisted = u.getId() != null;
+    if (persisted) {
+      // Find fresh entity for editing
+      user = userService.findById(u.getId()).get();
+    } else {
+      user = u;
+    }
+    cancel.setVisible(persisted);
+
+    // Bind customer properties to similarly named fields
+    // Could also use annotation or "manual binding" or programmatically
+    // moving values from fields to entities before saving
+    user.setPassword(null); // it's bcrypt, so we don't need to show it
+
+    binder.setBean(user);
+
+    setVisible(true);
+
+    // Focus first name initially
+    firstName.focus();
+  }
+
+  public void setChangeHandler(ChangeHandler h) {
+    // ChangeHandler is notified when either save or delete
+    // is clicked
+    changeHandler = h;
+  }
 
 }
--- src/main/java/by/ak/chat/view/UserView.java
+++ src/main/java/by/ak/chat/view/UserView.java
@@ -1,12 +1,20 @@
 package by.ak.chat.view;
 
+import by.ak.chat.component.Header;
+import by.ak.chat.component.UserEditor;
 import by.ak.chat.model.User;
 import by.ak.chat.service.UserService;
-import by.ak.chat.component.Header;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @PageTitle(UserView.TITLE)
 @Route(UserView.PATH)
@@ -15,17 +23,55 @@ public class UserView extends VerticalLayout {
   public static final String PATH = "/users";
   public static final String TITLE = "Users Management";
   private final UserService userService;
+  private final UserEditor editor;
   private final Header header;
   final Grid<User> grid;
 
-  public UserView(UserService userService, Header header) {
+  public UserView(UserService userService, UserEditor editor, Header header) {
     this.userService = userService;
+    this.editor = editor;
     this.header = header;
+
     add(header.init());
     grid = new Grid<>(User.class);
-    add(grid);
     grid.setColumns("id", "username", "email", "firstName", "lastName", "role", "created");
-    grid.setItems(userService.findAll());
+
+    Button addNewBtn = new Button("Add new user", VaadinIcon.PLUS.create(), event -> editor.editCustomer(new User()));
+
+    TextField filter = new TextField();
+    filter.setPlaceholder("Filter by last name");
+
+    // Hook logic to components
+    // Replace listing with filtered content when user changes filter
+    filter.setValueChangeMode(ValueChangeMode.EAGER);
+    filter.addValueChangeListener(e -> listCustomers(e.getValue()));
+
+    HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+
+    add(actions, grid, editor);
+
+    // Connect selected Customer to editor or hide if none is selected
+    grid.asSingleSelect().addValueChangeListener(e -> {
+      editor.editCustomer(e.getValue());
+    });
+
+    // Listen changes made by the editor, refresh data from backend
+    editor.setChangeHandler(() -> {
+      editor.setVisible(false);
+      listCustomers(filter.getValue());
+    });
+
+    // Initialize listing
+    listCustomers(null);
+  }
+
+  // Filter based on a full text search
+  private void listCustomers(String filterText) {
+    if (!hasText(filterText)) {
+      grid.setItems(userService.findAll());
+    } else {
+      grid.setItems(userService.findByLastName(filterText));
+    }
   }
 }
 
