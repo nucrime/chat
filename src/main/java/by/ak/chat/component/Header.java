607262cdae7c2ae0eb9716704ed4ed925b5bfbaa
Alexander Karankevich <givmemp3@gmail.com>
2021-12-13 01:33:14 +0300

    add toggle for dark theme

--- src/main/java/by/ak/chat/component/Header.java
+++ src/main/java/by/ak/chat/component/Header.java
@@ -6,9 +6,13 @@ import by.ak.chat.view.UserView;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.dom.ThemeList;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
@@ -34,6 +38,20 @@ public class Header {
 
     Button chatButton = new Button(CHAT, e -> current.navigate(ChatView.PATH));
     header.add(chatButton);
+
+    Button darkTheme = new Button(VaadinIcon.MOON_O.create(), click -> {
+      ThemeList themeList = UI.getCurrent().getElement().getThemeList();
+
+      if (themeList.contains(Lumo.DARK)) {
+        themeList.remove(Lumo.DARK);
+        click.getSource().setIcon(VaadinIcon.MOON_O.create());
+      } else {
+        themeList.add(Lumo.DARK);
+        click.getSource().setIcon(VaadinIcon.MOON.create());
+      }
+    });
+
+    header.add(darkTheme);
 
     Button logoutButton = new Button(LOG_OUT, e -> securityService.logout());
     header.add(logoutButton);
