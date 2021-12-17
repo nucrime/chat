54213af9761ad9a0dec85dc19f6e92f328a79d91
Alexander Karankevich <givmemp3@gmail.com>
2021-12-18 01:47:58 +0300

    refactor storage

--- src/main/java/by/ak/chat/model/Storage.java
+++ src/main/java/by/ak/chat/model/Storage.java
@@ -14,20 +14,17 @@ import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.time.Duration;
-import java.util.Queue;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Component
 @ApplicationScoped
 public class Storage {
-  public static final int ONE = 1;
-  public static final int TWO = 2;
-//  @Getter
-//  private final Queue<ChatMessage> messages = new ConcurrentLinkedQueue<>();
-  //map of queues
+  public static final int ONE_SECOND = 1;
+  public static final int HUNDRED_MILLISECONDS = 100;
+
   private final Chats chats = new Chats();
-  // todo change to private final ConcurrentLinkedDeque<ChatMessage> messages = new ConcurrentLinkedDeque<>();
-  //   use     messages.pollLast(); if messages.size() > 200
 
   private final ComponentEventBus eventBus = new ComponentEventBus(new Div());
   @Autowired
@@ -35,11 +32,8 @@ public class Storage {
 
   public void addMessage(String user, String message) {
     ChatMessage newMessage = new ChatMessage(user, message);
-//    messages.add(newMessage);
-//    eventBus.fireEvent(new ChatEvent());
     addAndSave(newMessage);
     fireEvent();
-//    chatService.save(newMessage).subscribe();
   }
 
   public Stream<String> chats() {
@@ -55,13 +49,13 @@ public class Storage {
     ChatMessage presence = new ChatMessage();
     presence.setText(message);
     addAndSave(presence);
-    delayFireEvent();
-//    Mono.delay(Duration.ofSeconds(TWO)).subscribe(e -> eventBus.fireEvent(new ChatEvent()));
-//    addAndSave(userJoined);
-//    delayFireEvent();
-//    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-//    executorService.schedule(() -> eventBus.fireEvent(new ChatEvent()), ONE, TimeUnit.SECONDS); // I want to ride my bicycle...
-//    chatService.save(userJoined).subscribe();
+    delayFireEvent(); // Delay fire event, so frontend can render message before user joined and scroll to bottom
+  }
+
+  public Optional<ChatMessage> getMessage(ChatMessage message) {
+    return chats.stream(message)
+      .filter(m -> message.getId().equals(m.getId()))
+      .findFirst();
   }
 
   public void updateMessage(ChatMessage message) {
@@ -81,25 +75,24 @@ public class Storage {
     }
   }
 
-  // todo listener for storage? or directly after firing event, :25#addMessage.
-  //  On new message asynchronously saves to mongo
   public Registration attachListener(ComponentEventListener<ChatEvent> listener) {
     return eventBus.addListener(ChatEvent.class, listener);
   }
 
   private void addAndSave(ChatMessage message) {
-    chatService.save(message).subscribe();
+    message.setChat(chatService.current());
     chats.add(message);
+    chatService.save(message).subscribe();
   }
 
-  public Queue<ChatMessage> getChat(String name) {
+  public List<ChatMessage> getChat(String name) {
     return chats.one(name);
   }
 
   public void fireEvent() {
-    eventBus.fireEvent(new ChatEvent());
+    Mono.delay(Duration.ofMillis(HUNDRED_MILLISECONDS)).subscribe(e -> eventBus.fireEvent(new ChatEvent()));
+    // default delay is 100ms. Enough for illusion of a real time, as well enough for frontend to render messages.
   }
-
   /*
 -------- __@      __@       __@       __@      __~@
 ----- _`\>,_    _`\>,_    _`\>,_    _`\>,_    _`\>,_     <--- reactive bicycle
@@ -107,7 +100,7 @@ public class Storage {
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
   private void delayFireEvent() {
-    Mono.delay(Duration.ofSeconds(TWO)).subscribe(e -> fireEvent());
+    Mono.delay(Duration.ofSeconds(ONE_SECOND)).subscribe(e -> fireEvent());
   }
 
   /*
@@ -115,9 +108,6 @@ public class Storage {
   * */
   @PostConstruct
   public void init() {
-/*    chatService.findAll()
-      .subscribeOn(Schedulers.boundedElastic())
-      .subscribe(messages::add); // migration to tailable cursor*/
     chatService.findAll().subscribe(chats::add);
   }
 }
