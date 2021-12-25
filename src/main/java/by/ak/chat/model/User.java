17d3401e640d5cb123877d87c8c60d69d080b7e4
Alexander Karankevich <givmemp3@gmail.com>
2021-12-26 00:24:38 +0300

    add dob field to user

--- src/main/java/by/ak/chat/model/User.java
+++ src/main/java/by/ak/chat/model/User.java
@@ -6,8 +6,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @Document
@@ -30,6 +34,10 @@ public class User {
   @Size(min = 3, max = 30)
   private String lastName;
 
+  @Past
+  @NotNull
+  private Date dob;
+
   @NotBlank
   @Email
   private String email;
@@ -38,6 +46,7 @@ public class User {
 
   private Role role = Role.USER;
 
+  @PastOrPresent
   private LocalDateTime created = LocalDateTime.now();
 //
 //  public String getRole() {
