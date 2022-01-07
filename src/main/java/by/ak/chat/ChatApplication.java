a6a978b6c15da3e79fc5f42dc60073db49fc5f5c
Alexander Karankevich <givmemp3@gmail.com>
2022-01-07 22:18:35 +0300

    update to vaadin 22

--- src/main/java/by/ak/chat/ChatApplication.java
+++ src/main/java/by/ak/chat/ChatApplication.java
@@ -1,11 +1,15 @@
 package by.ak.chat;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class ChatApplication {
+@Push
+public class ChatApplication  extends SpringBootServletInitializer implements AppShellConfigurator {
 
   public static void main(String[] args) {
     SpringApplication.run(ChatApplication.class, args);
--- src/main/java/by/ak/chat/view/ChatView.java
+++ src/main/java/by/ak/chat/view/ChatView.java
@@ -22,7 +22,6 @@ import com.vaadin.flow.component.messages.MessageInput;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -40,7 +39,6 @@ import static org.springframework.util.StringUtils.hasText;
 @Slf4j
 @Route(ChatView.PATH)
 @PageTitle(ChatView.TITLE)
-@Push
 public class ChatView extends VerticalLayout {
   public static final String CHAT_MESSAGE_TEMPLATE = "%s   **%s**: %s";
   public static final String PATH = "/";
