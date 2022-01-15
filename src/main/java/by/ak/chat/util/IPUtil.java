6d32a5d4c4cd360993c7be3febb1f095ab4c825e
Alexander Karankevich <givmemp3@gmail.com>
2022-01-15 04:07:15 +0300

    temporary file for google verification

--- src/main/java/by/ak/chat/util/IPUtil.java
+++ src/main/java/by/ak/chat/util/IPUtil.java
@@ -1,6 +1,5 @@
 package by.ak.chat.util;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import static com.google.common.net.HttpHeaders.X_FORWARDED_FOR;
 
 @Component
-@RequiredArgsConstructor
 public record IPUtil(HttpServletRequest request) {
 
   private static final int ORIGINATING_ADDRESS = 0;
---
+++ src/main/java/by/ak/chat/view/GoogleVerificationView.java
@@ -0,0 +1,13 @@
+package by.ak.chat.view;
+
+import com.vaadin.flow.component.HasText;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("googleec0225b0463cb519.html")
+public class GoogleVerificationView extends VerticalLayout implements HasText {
+
+  public GoogleVerificationView() {
+    add("google-site-verification: googleec0225b0463cb519.html");
+  }
+}
